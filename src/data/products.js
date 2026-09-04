5b8e2c85414d2efa4bf5b0d03d5be3248b0057c9
yigityilmaz16 <yigityilmaz196345@gmail.com>
2026-09-04 16:04:06 +0300

    feat: list initial return products

---
+++ src/data/products.js
@@ -0,0 +1,28 @@
+const initialProducts = [
+    {
+        id:1,
+        name: "Kablosuz Kulaklık",
+        store: "Teknoloji Mağazası",
+        purchaseDate: "2023-01-15",
+        returnPeriodDays: 30,
+        isReturned: false
+    },
+    {
+        id:2,
+        name: "Bluetooth Hoparlör",
+        store: "Elektronik Dünyası",
+        purchaseDate: "2023-02-10",
+        returnPeriodDays: 15,
+        isReturned: false
+    },
+    {
+        id:3,
+        name: "Akıllı Saat",
+        store: "Teknoloji Mağazası",
+        purchaseDate: "2023-03-20",
+        returnPeriodDays: 30,
+        isReturned: true
+    }
+]
+
+export default initialProducts;
