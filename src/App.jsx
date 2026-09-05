855ae4b95134dd858cfe17631541e4930cd62d32
yigityilmaz16 <yigityilmaz196345@gmail.com>
2026-09-05 12:24:12 +0300

    feat: toggle product return status

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import initialProducts from "./data/products.js"; 
 import "./App.css";
 import calculateRemainingDays from './utils/calculateRemainingDays.js'
+import {useState} from 'react'
 
 function App() {
-  
-     
+
+  const [products, setProducts] = useState(initialProducts);
+
+  function returnEventHandler(id) {
+    setProducts(currentProducts => currentProducts.map((product) => {
+      if (product.id === id) {
+        return { ...product, isReturned: !product.isReturned };
+      }
+      return product;
+    }));  
+  }
+
   return(
     <main>
       <h1>ReturnTrack</h1>
       <p>İade sürenizi kaçırmayın.</p>
       <ul className="ProductList">
-     {initialProducts.map((product) => {
+     {products.map((product) => {
       const remainingDays = calculateRemainingDays(
        product.purchaseDate,
        product.returnPeriodDays,
@@ -33,6 +44,9 @@ function App() {
       <p>Satın Alma Tarihi: {product.purchaseDate}</p>
       <p>İade Süresi (gün): {product.returnPeriodDays}</p>
       <p>Durum: {returnStatus }</p>
+      <button type="button" onClick={() => returnEventHandler(product.id)}>
+        {product.isReturned ? "İade Durumunu Geri Al" : "İade Edildi Olarak İşaretle"}
+      </button>
     </li>
   )
 })}
