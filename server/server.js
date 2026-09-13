0c1676c2feb904631985f1359d4fa2f6c48c9a98
yigityilmaz16 <yigityilmaz196345@gmail.com>
2026-09-13 15:22:15 +0300

    feat: add product creation endpoint

--- server/server.js
+++ server/server.js
@@ -9,6 +9,19 @@ app.get("/products", (req,res) =>{
     res.json(products);
 })
 
+app.post("/products", (req,res) =>{
+    const newProduct ={
+        id: Date.now(),
+        name: req.body.name,
+        store: req.body.store,
+        purchaseDate: req.body.purchaseDate,
+        returnPeriodDays: Number(req.body.returnPeriodDays),
+        isReturned: false
+    }
+    products.push(newProduct);
+    res.status(201).json(newProduct);
+
+})
 app.listen(3000, () =>{
     console.log("API dinleniyor");
 })
