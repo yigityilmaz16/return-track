088926180f1888d6aad183eccd08c60bdf616ec1
yigityilmaz16 <yigityilmaz196345@gmail.com>
2026-09-13 16:40:50 +0300

    feat: update products and return status

--- server/server.js
+++ server/server.js
@@ -77,6 +77,32 @@ app.patch("/products/:id/return-status", (req,res) =>{
     product.isReturned= isReturned
     res.status(200).json(product)
 })
+app.patch("/products/:id", (req,res) =>{
+    const product = products.find(
+        product => product.id === Number(req.params.id)
+    )
+    if(!product){
+       res.status(404).json({
+         message: "Ürün Bulunamadı"
+       })
+       return;
+    }
+    const { name, store, purchaseDate, returnPeriodDays } = req.body
+    const cleanName = typeof name === 'string' ? name.trim() : ''
+    const cleanStore = typeof store === 'string' ? store.trim() : ''
+    const cleanReturnPeriodDays = Number(returnPeriodDays)
+     if(!cleanStore || !cleanName || !purchaseDate || cleanReturnPeriodDays <= 0 || !Number.isFinite(cleanReturnPeriodDays)){
+        res.status(400).json({
+            message : "Tüm Alanlar Dolu Olmalı Veya Sayılar Pozitif Olmalı!"
+        })
+        return;
+    }
+    product.name= cleanName
+    product.store= cleanStore
+    product.purchaseDate= purchaseDate
+    product.returnPeriodDays= cleanReturnPeriodDays
+    res.status(200).json(product)
+})
 app.listen(3000, () =>{
     console.log("API dinleniyor");
 })
