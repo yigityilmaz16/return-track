bf98f4f48b2907dc9ac69a4f9b52c9e9ae4f6e3a
yigityilmaz16 <yigityilmaz196345@gmail.com>
2026-09-15 14:00:11 +0300

    feat: allow frontend API requests with CORS

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import productRoute from './routes/productRoute.js'
+import cors from 'cors'
 const app= express();
 app.use(express.json());
+app.use(cors({ origin: 'http://localhost:5173'}))
 app.use('/products', productRoute)
+
 
 
 app.listen(3000, () =>{
