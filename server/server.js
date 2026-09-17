84c2839ed69f9c468cc2974f9f34cacffb353d87
yigityilmaz16 <yigityilmaz196345@gmail.com>
2026-09-17 13:57:49 +0300

    chore: configure backend environment variables

--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import productRoute from './routes/productRoute.js'
 import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
+const PORT =process.env.PORT || 3000
+const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:5173'
 const app= express();
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173'}))
+app.use(cors({ origin: CLIENT_ORIGIN}))
 app.use('/products', productRoute)
 
 
 
-app.listen(3000, () =>{
-    console.log("API dinleniyor");
+
+app.listen(PORT, () =>{
+    console.log(PORT);
 })
