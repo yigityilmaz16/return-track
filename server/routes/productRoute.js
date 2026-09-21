7bf62a81208c914cf03340e7c1c1a1f1b9c6d637
yigityilmaz16 <yigityilmaz196345@gmail.com>
2026-09-21 12:12:11 +0300

    feat: fetch and delete products by MongoDB id

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -33,29 +33,37 @@ router.post("/", async (req,res) =>{
 }
 
 })
-router.get("/:id", (req,res) =>{
-    const product = products.find(
-        product => product.id === Number(req.params.id)
-    )
-    if(!product){
+router.get("/:id", async (req,res) =>{
+   try{
+    const data= await Product.findById(req.params.id)
+    if(!data){
         res.status(404).json({
             message: "Ürün Bulunamadı"
         })
         return;
     }
-    res.status(200).json(product)
+    res.status(200).json(data)
+}catch(error){
+    res.status(500).json({
+        message:"Hata"
+    })
+}
 })
-router.delete("/:id", (req,res) =>{
-    const index= products.findIndex(
-        product => product.id === Number(req.params.id)
-    )
-    if (index === -1) {
-    return res.status(404).json({
-        message: "Ürün Bulunamadı"
-    });
+router.delete("/:id", async (req,res) =>{
+   try{
+    const data = await Product.findByIdAndDelete(req.params.id)
+    if(!data){
+        res.status(404).json({
+            message: "ürün bulunamadı"
+        })
+        return;
     }
-    const [deletedProduct] = products.splice(index, 1)
-    res.status(200).json(deletedProduct)
+    res.status(200).json(data)
+}catch(error){
+    res.status(500).json({
+        message: "hata"
+    })
+}
 
 })
 router.patch("/:id/return-status", (req,res) =>{
