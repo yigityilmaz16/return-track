4c70ffa95e1e7a931c53bd267ab1eeb2d7a629a9
yigityilmaz16 <yigityilmaz196345@gmail.com>
2026-09-21 11:50:32 +0300

    feat: integrate MongoDB and add product model

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import productRoute from './routes/productRoute.js'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import connectDatabase from './config/database.js';
 dotenv.config()
 const PORT =process.env.PORT || 3000
 const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:5173'
@@ -9,10 +10,19 @@ const app= express();
 app.use(express.json());
 app.use(cors({ origin: CLIENT_ORIGIN}))
 app.use('/products', productRoute)
-
-
-
-
-app.listen(PORT, () =>{
+async function startServer() {
+   try{
+    await connectDatabase()
+    app.listen(PORT, () =>{
     console.log(PORT);
 })
+   }catch(error){
+    console.log("hata")
+    process.exit(1)
+   }
+}
+startServer()
+
+
+
+
